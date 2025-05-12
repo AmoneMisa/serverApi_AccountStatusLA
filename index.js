bd4c8cd26b52f13e434829c874f9097072a7940d
WhitesLove <kubai.rita5@gmail.com>
2025-05-12 16:14:46 +0300

    Remove subs api

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ import cors from 'cors';
 import { connectToDatabase } from './server/db.js';
 
 import userRoutes from './server/routes/users.js';
-import subscriptionRoutes from './server/routes/subscriptions.js';
 
 const index = express();
 const PORT = process.env.PORT || 3001;
@@ -12,7 +11,6 @@ index.use(cors());
 index.use(express.json());
 
 index.use('/api/users', userRoutes);
-index.use('/api/subscriptions', subscriptionRoutes);
 
 connectToDatabase().then(() => {
   index.listen(PORT, () => console.log(`Server running on port ${PORT}`));
--- server/models/Subscription.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-
-const subscriptionSchema = new mongoose.Schema({
-    from: String,
-    to: String
-});
-
-export default mongoose.model('Subscription', subscriptionSchema);
