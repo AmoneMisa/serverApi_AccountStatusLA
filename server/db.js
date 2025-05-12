12123bdbac604599568ea3127de314179203ec85
WhitesLove <kubai.rita5@gmail.com>
2025-05-12 16:51:33 +0300

    Added docker-compose

--- server/db.js
+++ server/db.js
@@ -2,7 +2,6 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config();
 
-console.log(process.env.MONGO_URI)
 export async function connectToDatabase() {
     await mongoose.connect(process.env.MONGO_URI);
     console.log('MongoDB connected');
